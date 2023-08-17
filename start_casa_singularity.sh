222acd1aa4922405e4a808c0a5035ec80af9584b
Christof Buchbender <buchbend@ph1.uni-koeln.de>
2023-08-17 15:24:45 +0200

    Added singularity

--- start_casa_singularity.sh
+++ start_casa_singularity.sh
@@ -3,6 +3,16 @@
 
 # Ensure local access to the X server
 xhost +local:
-
+SINGULARITYENV_LANG=$LANG \
+SINGULARITYENV_LANGUAGE=$LANGUAGE \
+SINGULARITYENV_LC_ALL=$LC_ALL \
+SINGULARITYENV_LC_CTYPE=$LC_CTYPE \
 # Run the Singularity container
-singularity run --bind $PWD:/data name_of_your_container.sif
+singularity run \
+--bind $PWD:/data \
+--bind /dev/fuse:/dev/fuse \
+--bind /tmp/.X11-unix:/tmp/.X11-unix \
+--bind /usr/share/locale:/usr/share/locale \
+--writable-tmpfs \
+casa_1.sif 
+
